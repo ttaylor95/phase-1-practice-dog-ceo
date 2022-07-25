d038455610c9784d43e68332c736bd8071c68af9
Teanna Taylor <teannamtaylor95@gmail.com>
2022-07-24 22:14:02 -0700

    commit tree d900fa14de00c54d03928bc40100e2d20962d514

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('%c HI', 'color: firebrick')
+//console.log('%c HI', 'color: firebrick')
+
+//write javascript that fetches the images, parses the response as json
+
+const container = document.querySelector("#dog-image-container")
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+fetch(imgUrl)
+.then( resp => resp.json())
+.then(images =>{
+    const imgs = imgs.map((img) =>{
+        let i = `<img src=${img}>`
+        return i
+    })
+
+    imgsArray.forEach(element =>{
+        container.innerHTML += element 
+    })
+})
